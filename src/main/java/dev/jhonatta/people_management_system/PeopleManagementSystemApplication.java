b52f3830a4bf28cb0052d56b3ba073eeacefaef8
Jhonatta <jhonattasantos27@gmaol.com>
2025-03-16 19:07:52 -0300

    chore: add environment variables for database configuration and access

--- src/main/java/dev/jhonatta/people_management_system/PeopleManagementSystemApplication.java
+++ src/main/java/dev/jhonatta/people_management_system/PeopleManagementSystemApplication.java
@@ -1,5 +1,6 @@
 package dev.jhonatta.people_management_system;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PeopleManagementSystemApplication {
 
 	public static void main(String[] args) {
+		//Load the variables from the .env file.
+		Dotenv dotenv = Dotenv.configure().load();
+
+		//Set the environment variables in the system.
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+
 		SpringApplication.run(PeopleManagementSystemApplication.class, args);
 	}
 
