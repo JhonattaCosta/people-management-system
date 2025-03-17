5a6e061c9284503505f8a3166d5b9c54247a7e4b
Jhonatta <jhonattasantos27@gmaol.com>
2025-03-17 20:53:18 -0300

    feat: implement UserController endpoints and UserService methods
    -Added POST /create endpoint in UserController to create a new User
    
    -Added GET /list endpoint in UserController to fetch all users
    
    -Added GET /list/{id} endpoint in UserController to fetch a user by ID
    
    -Added POST /update/{id} endpoint in UserController to update user
    
    -Added DELETE /delete/{id} endpoint in UserController to delete user by ID

--- src/main/java/dev/jhonatta/people_management_system/controller/UserController.java
+++ src/main/java/dev/jhonatta/people_management_system/controller/UserController.java
@@ -1,4 +1,70 @@
 package dev.jhonatta.people_management_system.controller;
 
+import dev.jhonatta.people_management_system.dto_mapper.UserDTO;
+import dev.jhonatta.people_management_system.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createUser(@RequestBody UserDTO user){
+        UserDTO newUser = userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("User created successfully! " + newUser.getName() + "(ID): " + newUser.getId());
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<UserDTO>> listUsers(){
+        List<UserDTO> users = userService.listUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/list/{id}")
+    public ResponseEntity<?> listUserById (@PathVariable Long id){
+        UserDTO user = userService.listUserById(id);
+        if(user != null){
+            return ResponseEntity.ok(user);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("User with id: " + id +" not found");
+        }
+    }
+
+    @PatchMapping("/update/{id}")
+    public ResponseEntity<?> updateUsers(@PathVariable Long id, @RequestBody UserDTO userUpdated){
+        UserDTO user = userService.listUserById(id);
+        if (user != null){
+            return ResponseEntity.status(HttpStatus.ACCEPTED)
+                    .body(userService.updateUser(id,userUpdated));
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("User with id: " + id + " not found");
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteUserById(@PathVariable Long id){
+        UserDTO user = userService.listUserById(id);
+        if (user != null){
+            userService.userDeleteById(id);
+            return ResponseEntity.ok("User with id: "+ id +" was deleted successfully");
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("User with id: " + id + " not found");
+        }
+    }
+
+    //Constructor
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
 }
--- src/main/java/dev/jhonatta/people_management_system/service/UserService.java
+++ src/main/java/dev/jhonatta/people_management_system/service/UserService.java
@@ -1,4 +1,79 @@
 package dev.jhonatta.people_management_system.service;
 
+import dev.jhonatta.people_management_system.dto_mapper.UserDTO;
+import dev.jhonatta.people_management_system.dto_mapper.UserMapper;
+import dev.jhonatta.people_management_system.model.UserModel;
+import dev.jhonatta.people_management_system.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+
+    //Create User
+    public UserDTO createUser(UserDTO userDTO){
+        UserModel user = userMapper.map(userDTO);
+        user = userRepository.save(user);
+        return userMapper.map(user);
+    }
+
+    //List Users
+    public List<UserDTO> listUsers(){
+    List<UserModel> users = userRepository.findAll();
+    return users.stream()
+            .map(userMapper::map)
+            .collect(Collectors.toList());
+    }
+
+    //List Users by ID
+    public UserDTO listUserById(Long id){
+        Optional<UserModel> userById = userRepository.findById(id);
+        return userById.map(userMapper::map).orElse(null);
+    }
+
+    //Change Users
+    public UserDTO updateUser(Long id, UserDTO userDTO){
+        Optional<UserModel> userExistingOpt = userRepository.findById(id);
+        if(userExistingOpt.isPresent()){
+            UserModel userExiting = userExistingOpt.get();
+
+            if(userDTO.getName() != null){
+                userExiting.setName(userDTO.getName());
+            }
+            if(userDTO.getEmail() != null){
+                userExiting.setEmail(userDTO.getEmail());
+            }
+            if(userDTO.getAge() != 0){
+                userExiting.setAge(userDTO.getAge());
+            }
+            if(userDTO.getCpf() != null){
+                userExiting.setCpf(userDTO.getCpf());
+            }
+            if(userDTO.getImgUrl() != null){
+                userExiting.setImgUrl(userDTO.getImgUrl());
+            }
+            //Save user changes
+            UserModel userSave = userRepository.save(userExiting);
+            return userMapper.map(userSave);
+        }
+        return null;
+    }
+
+    //Delete User
+    public void userDeleteById (Long id){
+        userRepository.deleteById(id);
+    }
+
+    //Constructor
+    public UserService(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
 }
