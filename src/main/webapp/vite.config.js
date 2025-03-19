aaa1583983cda7186173c9042eb67095ad88697e
Jhonatta <jhonattasantos27@gmaol.com>
2025-03-19 11:44:25 -0300

    fix: Added environment variables to hide sensitive information.

--- src/main/webapp/vite.config.js
+++ src/main/webapp/vite.config.js
@@ -1,11 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build:{
-  outDir:'../resources/static',
-  emptyOutDir: true,
-  },
+export default defineConfig(({ mode }) => {
+    // "Load the environment variables from the .env file.
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [react()],
+        server: {
+            proxy: {
+                '/api': {
+                    target: env.VITE_API_URL, // Use the environment variable for the target.
+                    changeOrigin: true,
+                },
+            },
+        },
+        build: {
+            outDir: env.VITE_OUT_DIR, // Use the environment variable for the outDir.
+            emptyOutDir: true,
+        },
+    };
 });
